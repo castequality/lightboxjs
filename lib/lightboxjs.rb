a34f35d067ad361942bc9b66277c472ff14553bd
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 17:29:46 -0500

    complies with sprockets_better_errors

--- lib/lightboxjs.rb
+++ lib/lightboxjs.rb
@@ -1,6 +1,11 @@
 module LightboxJs
   module Rails
     class Engine < ::Rails::Engine
+      initializer :append_dependent_assets_path, :group => :all do |app|
+        app.config.assets.precompile += %w( lightbox.js )
+        app.config.assets.precompile += %w( lightbox/loading.gif )
+        app.config.assets.precompile += %w( lightbox/close.png )
+      end
     end
   end
 end
